479da9e9df9c3ff2b65712aa1f562f7492c3649b
aiden <aiden.langba@sonicbolt.com>
2022-06-30 19:10:19 +0530

    update

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,25 @@
-import React from "react";
-import "./Button.css";
+import React from "react"
+import "./Button.css"
 
 export interface IButtonProps
-  extends React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > {
-  /**
-   * Button label
-   */
-  label?: string;
+   extends React.DetailedHTMLProps<
+      React.ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+   > {
+   /**
+    * Button label
+    */
+   label?: string
 }
 
 const Button: React.FC<IButtonProps> = (props) => {
-  const { children, style, label } = props;
-  let _style: React.CSSProperties = style || {};
-  return (
-    <button type="button" className="sb__btn" style={_style}>
-      {label ? label : children}
-    </button>
-  );
-};
-export default Button;
+   const { children, style, label } = props
+   const content = label || children || "Button"
+   let _style: React.CSSProperties = style || {}
+   return (
+      <button type="button" className="sb__btn" style={_style}>
+         {content}
+      </button>
+   )
+}
+export default Button
